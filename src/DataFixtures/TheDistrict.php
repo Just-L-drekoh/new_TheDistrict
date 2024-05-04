d215647cf3c73cd43afc5492063dba6cb43b02f1
logan <loloherbo.drekoh@gmail.com>
2024-05-04 09:37:56 +0200

    fixtures

--- src/DataFixtures/TheDistrict.php
+++ src/DataFixtures/TheDistrict.php
@@ -16,94 +16,78 @@ class TheDistrict extends Fixture
     {
         include 'the_district.php';
 
+    foreach ($utilisateur as $user) {
+        $userDB = new Utilisateur();
+        $userDB
+            ->setEmail($user['email'])
+            ->setPassword($user['password'])
+            ->setNom($user['nom'])
+            ->setPrenom($user['prenom'])
+            ->setTelephone($user['telephone'])
+            ->setAdresse($user['adresse'])
+            ->setCp($user['cp'])
+            ->setVille($user['ville']);
 
-        foreach ($utilisateur as $user) {
-            $userDB = new Utilisateur();
-            $userDB
-                ->setEmail($user['email'])
-                ->setPassword($user['password'])
-                ->setNom($user['nom'])
-                ->setPrenom($user['prenom'])
-                ->setTelephone($user['telephone'])
-                ->setAdresse($user['adresse'])
-                ->setCp($user['cp'])
-                ->setVille($user['ville']);
+        $manager->persist($userDB);
+    }
+    $manager->flush();
 
-            $manager->persist($userDB);
-        }
-        $manager->flush();
+    $userRepo = $manager->getRepository(Utilisateur::class);
 
-        foreach ($categorie as $cat) {
-            $categorieDB = new Categorie();
-            $categorieDB
-                ->setLibelle($cat['libelle'])
-                ->setImage($cat['image'])
-                ->setActive($cat['active']);
-            $manager->persist($categorieDB);
-        }
-        $manager->flush();
-
-
-        $categorieRepo = $manager->getRepository(Categorie::class);
-
-        // Insérer les plats
-        foreach ($plat as $p) {
-            $platDB = new Plat();
-            $platDB
-                ->setLibelle($p['libelle'])
-                ->setDescription($p['description'])
-                ->setPrix($p['prix'])
-                ->setImage($p['image'])
-                ->setActive($p['active']);
-            $id = $p['id_categorie'];
-            $categorie = $categorieRepo->find($id);
-            $platDB->setCategorie($categorie);
-            $manager->persist($platDB);
-        }
-        $manager->flush();
-
-        $platRepo = $manager->getRepository(Plat::class);
-
-        foreach ($detail as $d) {
-            $detailDB = new Detail();
-            $detailDB
-                ->setQuantite($d['quantite']);
-            $id = $d['id_plat'];
-            $plat = $platRepo->find($id);
-            $detailDB->setPlat($plat);
-            $manager->persist($detailDB);
-        }
-        $manager->flush();
-
-
-        $detailRepo = $manager->getRepository(Detail::class);
-        $userRepo = $manager->getRepository(Utilisateur::class);
-        $user = $userRepo->find(1);
         foreach ($commande as $comm) {
             $commandeDB = new Commande();
             $dateCommande = new \DateTimeImmutable($comm['date_commande']);
             $commandeDB
                 ->setDateCommande($dateCommande)
                 ->setTotal($comm['total'])
-                ->setEtat($comm['etat'])
-                ->setUtilisateur($user);
-            $id = $comm['id_detail'];
-            $detail = $detailRepo->find($id);
-            $commandeDB->addDetail($detail);
+                ->setEtat($comm['etat']);
+                $user = $userRepo->find($comm['utilisateur_id']);
+            $commandeDB->setUtilisateur($user);
             $manager->persist($commandeDB);
+    }
+        $manager->flush();
+
+        foreach ($categorie as $categorie) {
+            $categorieDB = new Categorie();
+            $categorieDB
+                ->setLibelle($categorie['libelle'])
+                ->setImage($categorie['image'])
+                ->setActive($categorie['active']);
+            $manager->persist($categorieDB);
+        }
+        $manager->flush();
+        $categorieRepo = $manager->getRepository(Categorie::class);
+
+        foreach ($plat as $plat) {
+            $platDB = new Plat();
+            $platDB
+                ->setLibelle($plat['libelle'])
+                ->setImage($plat['image'])
+                ->setActive($plat['active'])
+                ->setDescription($plat['description'])
+                ->setPrix($plat['prix']);
+            $categorie = $categorieRepo->find($plat['categorie_id']);
+            $platDB->setCategorie($categorie);
+            $manager->persist($platDB);
         }
         $manager->flush();
 
+        $platRepo = $manager->getRepository(Plat::class);
         $commandeRepo = $manager->getRepository(Commande::class);
-        foreach ($utilisateur as $util) {
-            $id_user = $util['id'];
-            $id_commande = $util['id_commande'];
-            $user = $userRepo->find($id_user);
-            $order = $commandeRepo->find($id_commande);
-            $user->addCommande($order);
-            $order->setUtilisateur($user);
-        }
 
+        foreach ($detail as $detail) {
+            $detailDB = new Detail();
+            $detailDB
+                ->setQuantite($detail['quantite']);
+            $plat = $platRepo->find($detail['plat_id']);
+            $detailDB->setPlat($plat);
+            $commande = $commandeRepo->find($detail['commande_id']);
+            $detailDB->setCommande($commande);
+            $manager->persist($detailDB);
+        }
         $manager->flush();
     }
+
+
+
 }
