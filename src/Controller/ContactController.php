2105f1ced958301c09e9f6cf61b42ae82e21e9e2
logan <loloherbo.drekoh@gmail.com>
2024-05-08 17:46:24 +0200

    08.05

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,27 +3,35 @@
 namespace App\Controller;
 
 
-
-namespace App\Controller;
-
-use MailService;
+use App\Service\MailService;
 use App\Form\ContactFormType;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
 
 class ContactController extends AbstractController
 {
 
-    public function contact(Request $request, MailService $ms): Response
+
+
+    public function contact(Request $request, MailService $ms)
     {
 
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
 
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $ms->sendEmail($data);
+            $ms->sendEmailConfirmation($data);
+            return $this->redirectToRoute('home');
+        }
 
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
-            'form' => $form
+            'form' => $form->createView(),
         ]);
     }
 }
