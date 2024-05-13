d003452159a27ce435413e92b6d66037caf58f1c
logan <loloherbo.drekoh@gmail.com>
2024-05-13 07:01:57 +0200

    13.05

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -7,20 +7,51 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 class PanierController extends AbstractController
 {
 
+
+    public function addPanier(Request $request, PlatRepository $plat, SessionInterface $session): Response
+    {
+        $user = $this->getUser();
+
+        $id = $request->attributes->get('id');
+
+        $plats = $plat->find(['id' => $id]);
+
+        $session->set('addPanier', $plats);
+
+
+
+        return $this->render('panier/index.html.twig', [
+            'user' => $user,
+            'sessions' => $session,
+        ]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     #[IsGranted("ROLE_USER")]
-    public function listPanier(Request $request, PlatRepository $plat): Response
+    public function listPanier(Request $request, PlatRepository $plat, SessionInterface $session): Response
     {
 
         $user = $this->getUser();
-        $libelle = $request->query->get('libelle');
 
-        $plats = $plat->findOneBy(['libelle' => $libelle]);
 
         return $this->render('panier/index.html.twig', [
             'user' => $user,
