bad88cb2853dbc22b73599f0c0f0560405090cd2
logan <loloherbo.drekoh@gmail.com>
2024-05-30 13:31:12 +0200

    fixturesv2

--- src/DataFixtures/TheDistrict.php
+++ src/DataFixtures/TheDistrict.php
@@ -14,6 +14,7 @@ class TheDistrict extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        // include base 
         include 'districtbase.php';
 
         foreach ($utilisateur as $user) {
